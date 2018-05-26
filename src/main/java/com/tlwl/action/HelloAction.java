dcab39be62903e6fc545bc2feb8aec5ae0401a99
zhoutk <zhoutk@189.cn>
2018-05-26 16:06:09 +0800

    can run base get request.

---
+++ src/main/java/com/tlwl/action/HelloAction.java
@@ -0,0 +1,19 @@
+package com.tlwl.action;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/hello")
+public class HelloAction {
+
+    @GET
+    @Path("{name}")
+    @Produces(MediaType.TEXT_PLAIN)
+    //访问路径 /hello/everyone
+    public String hello(@PathParam("name") String name){
+        return "hello wolrd! "+name;
+    }
+}
--- src/main/java/com/tlwl/main/App.java
+++ src/main/java/com/tlwl/main/App.java
@@ -1,13 +1,19 @@
 package com.tlwl.main;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.net.URI;
+import org.eclipse.jetty.server.Server;//jetty包
+import javax.ws.rs.core.UriBuilder;//jersey-container-jetty-http
+import org.glassfish.jersey.server.ResourceConfig;//
+import org.glassfish.jersey.jetty.JettyHttpContainerFactory;//jersey-container-jetty-http  + jersey-hk2这两个坐标缺一不可
+public class App {
+
+    public static void main(String[] args) throws Exception {
+
+        URI baseUri = UriBuilder.fromUri("http://localhost/").port(5000).build();
+        ResourceConfig config = new ResourceConfig(com.tlwl.action.HelloAction.class);//jersey-server
+        Server server = JettyHttpContainerFactory.createServer(baseUri, config);//pom
+        System.out.println("JettyServer Running on 127.0.0.1:5000");
+        server.start();
+
     }
 }
