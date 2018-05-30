e453fbbed120c04ec733bde91e9ede68b70128e9
zhoutk <zhoutk@189.cn>
2018-05-30 11:26:17 +0800

    execute return info adjust.

--- src/main/java/com/tlwl/db/mysql/MysqlDao.java
+++ src/main/java/com/tlwl/db/mysql/MysqlDao.java
@@ -12,9 +12,10 @@ public class MysqlDao {
     private static JSONObject dbConfs = GlobalConst.CONFIGS.getJSONObject("db_mysql_config");
     private static int PoolMaxSize = dbConfs.getInt("db_conn_limit");
     private static Connection conn[] = new Connection[PoolMaxSize];
+
     private static Connection createConnection() {
         int index = RandomUtils.nextInt(0, dbConfs.getInt("db_conn_limit"));
-        if(conn[index] == null) {
+        if (conn[index] == null) {
             try {
                 conn[index] = DriverManager.getConnection(
                         "jdbc:mysql://" + dbConfs.getString("db_host") + ":" + dbConfs.getInt("db_port") + "/" + dbConfs.getString("db_name") + "?" +
@@ -29,16 +30,16 @@ public class MysqlDao {
         return conn[index];
     }
 
-    public static JSONObject select(String tablename, JSONObject params, String [] fields){  //String tablename, Object params, String [] fields
+    public static JSONObject select(String tablename, JSONObject params, String[] fields) {  //String tablename, Object params, String [] fields
         return query(tablename, params, fields, null, null);
     }
 
-    private static JSONObject query(String tablename, JSONObject params, String[] fields, String sql, JSONArray values){
-        if(values == null)
+    private static JSONObject query(String tablename, JSONObject params, String[] fields, String sql, JSONArray values) {
+        if (values == null)
             values = new JSONArray();
         String where = "";
         Boolean is_search = false;
-        if(params.has("search")){
+        if (params.has("search")) {
             is_search = true;
             params.remove("search");
         }
@@ -48,26 +49,26 @@ public class MysqlDao {
         String like = params.has("lks") ? params.getString("lks") : "";
 
         Iterator<String> ks = params.keys();
-        while( ks.hasNext()){
+        while (ks.hasNext()) {
             String key = ks.next();
             String value = params.getString(key);
-            if(where != "")
+            if (where != "")
                 where += " and ";
 
-                if(is_search){
-                    where += key + " like ? ";
-                    values.put("%" + value + "%");
-                }else{
-                    where += key + " = ? ";
-                    values.put(value);
-                }
+            if (is_search) {
+                where += key + " like ? ";
+                values.put("%" + value + "%");
+            } else {
+                where += key + " = ? ";
+                values.put(value);
+            }
         }
 
-        if(tablename == "QuerySqlSelect"){
+        if (tablename == "QuerySqlSelect") {
             sql += "";
-        }else{
+        } else {
             sql = "SELECT * FROM " + tablename;
-            if(where != ""){
+            if (where != "") {
                 sql += " WHERE " + where;
             }
         }
@@ -75,78 +76,79 @@ public class MysqlDao {
         return execQuery(sql, values);
     }
 
-    private static JSONObject execQuery(String sql, JSONArray values){
+    private static JSONObject execQuery(String sql, JSONArray values) {
         Boolean flag = true;
         int errorCode = 0;
         String errorMessage = "";
         Connection conn = createConnection();
-        if(conn == null)
-            return null;
-        PreparedStatement stmt = null;
-        ResultSet rs = null;
         JSONArray json = new JSONArray();
-        Iterator ks = values.iterator();
-        DBParams ps = new DBParams();
-        int m = 0;
-        while (ks.hasNext()) {
-            ps.addParam(values.get(m));
-            m++;
-            ks.next();
-        }
-        try {
-            stmt = conn.prepareStatement(sql);
-            ps.prepareStatement(stmt);
-            if(stmt.execute()){
-                rs =  stmt.getResultSet();
-                ResultSetMetaData rsmd = rs.getMetaData();
-                int columnCount = rsmd.getColumnCount();
-                while(rs.next()){
-                    JSONObject jo = new JSONObject();
-                    for (int i = 1; i <= columnCount; i++) {
-                        String theKey = rsmd.getColumnLabel(i);
-                        if(theKey.endsWith("_json")){
-                            String theValue = rs.getObject(i) == null ? null : rs.getObject(i).toString();
-                            if(theValue != null && theValue.startsWith("["))
-                                jo.put(theKey, new JSONArray(theValue));
-                            else if(theValue != null)
-                                jo.put(theKey, new JSONObject(theValue));
-                        }else{
-                            jo.put(theKey, rs.getObject(i));
+        if (conn != null) {
+            PreparedStatement stmt = null;
+            ResultSet rs = null;
+            Iterator ks = values.iterator();
+            DBParams ps = new DBParams();
+            int m = 0;
+            while (ks.hasNext()) {
+                ps.addParam(values.get(m));
+                m++;
+                ks.next();
+            }
+            try {
+                stmt = conn.prepareStatement(sql);
+                ps.prepareStatement(stmt);
+                if (stmt.execute()) {
+                    rs = stmt.getResultSet();
+                    ResultSetMetaData rsmd = rs.getMetaData();
+                    int columnCount = rsmd.getColumnCount();
+                    while (rs.next()) {
+                        JSONObject jo = new JSONObject();
+                        for (int i = 1; i <= columnCount; i++) {
+                            String theKey = rsmd.getColumnLabel(i);
+                            if (theKey.endsWith("_json")) {
+                                String theValue = rs.getObject(i) == null ? null : rs.getObject(i).toString();
+                                if (theValue != null && theValue.startsWith("["))
+                                    jo.put(theKey, new JSONArray(theValue));
+                                else if (theValue != null)
+                                    jo.put(theKey, new JSONObject(theValue));
+                            } else {
+                                jo.put(theKey, rs.getObject(i));
+                            }
                         }
+                        json.put(jo);
                     }
-                    json.put(jo);
+                    System.out.println("SQL: " + sql + "; VALUES: " + values.toString());
                 }
-                System.out.println("SQL: " + sql + "; VALUES: " + values.toString());
-            }
-        }
-        catch (SQLException ex){
-            flag = false;
-            errorCode = ex.getErrorCode();
-            errorMessage = ex.getMessage();
-            System.out.println("SQL: " + sql + "; Code: " + ex.getErrorCode() + "; Message: " + ex.getMessage());
-        }
-        finally {
-            if (rs != null) {
-                try {
-                    rs.close();
-                } catch (SQLException sqlEx) {
-                    sqlEx.printStackTrace();
+            } catch (SQLException ex) {
+                flag = false;
+                errorCode = ex.getErrorCode();
+                errorMessage = ex.getMessage();
+                System.out.println("SQL: " + sql + "; Code: " + ex.getErrorCode() + "; Message: " + ex.getMessage());
+            } finally {
+                if (rs != null) {
+                    try {
+                        rs.close();
+                    } catch (SQLException sqlEx) {
+                        sqlEx.printStackTrace();
+                    }
                 }
-            }
 
-            if (stmt != null) {
-                try {
-                    stmt.close();
-                } catch (SQLException sqlEx) {
-                    sqlEx.printStackTrace();
+                if (stmt != null) {
+                    try {
+                        stmt.close();
+                    } catch (SQLException sqlEx) {
+                        sqlEx.printStackTrace();
+                    }
                 }
             }
+        } else {
+            errorCode = 4221;
+            errorMessage = "The database connect error.";
         }
         JSONObject result = new JSONObject();
-        if(flag){
+        if (flag) {
             result.put("code", 200);
             result.put("rows", json);
-        }else{
+        } else {
             result.put("code", 500);
             result.put("errcode", errorCode);
             result.put("message", errorMessage);
