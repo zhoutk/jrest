168b47a05e2b752164b9e0ab810632bcdf44a06c
zhoutk <zhoutk@189.cn>
2018-06-01 17:38:27 +0800

    jwt tools func is ok.

--- src/main/java/com/tlwl/main/GlobalConst.java
+++ src/main/java/com/tlwl/main/GlobalConst.java
@@ -6,8 +6,24 @@ import com.fasterxml.uuid.impl.TimeBasedGenerator;
 import com.tlwl.utils.Tools;
 import org.json.JSONObject;
 
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import org.apache.commons.codec.binary.Base64;
+
 public class GlobalConst {
     private static TimeBasedGenerator uuidGenerator = Generators.timeBasedGenerator(EthernetAddress.fromInterface());
+    private static SecretKey generalKey(String jwt_secret) {
+        SecretKey key = null;
+        try {
+            String stringKey = jwt_secret;
+            byte[] encodedKey = Base64.decodeBase64(stringKey);
+            key = new SecretKeySpec(encodedKey, 0, encodedKey.length, "AES");
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+        return key;
+    }
+
     public final static JSONObject CONFIGS = Tools.jsonRead("./configs.json");
 
     private final static JSONObject CONSTS = Tools.jsonRead("./GlobalConst.json");
@@ -20,4 +36,8 @@ public class GlobalConst {
     public static String uuid(){
         return uuidGenerator.generate().toString().split("-")[0];
     }
+
+    public final static SecretKey JWT_KEY = generalKey(CONFIGS.getString("jwt_secret"));
+
+
 }
--- src/main/java/com/tlwl/utils/Tools.java
+++ src/main/java/com/tlwl/utils/Tools.java
@@ -3,11 +3,34 @@ package com.tlwl.utils;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+
+import com.tlwl.main.GlobalConst;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.json.JSONObject;
 import org.json.JSONException;
 
 public class Tools {
-    public static JSONObject jsonRead(String file){
+    public static String encodeJsonWebToken(JSONObject payload) {
+        return Jwts.builder()
+                .setPayload(payload.toString())
+                .signWith(SignatureAlgorithm.HS512, GlobalConst.JWT_KEY)
+                .compact();
+    }
+
+    public static JSONObject decodeJsonWebToken(String pass) {
+        Claims payload = null;
+        try {
+            payload = Jwts.parser().setSigningKey(GlobalConst.JWT_KEY)
+                    .parseClaimsJws(pass).getBody();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return new JSONObject(payload);
+    }
+
+    public static JSONObject jsonRead(String file) {
         JSONObject dataJson = null;
         try {
             BufferedReader br = new BufferedReader(new FileReader(file));
